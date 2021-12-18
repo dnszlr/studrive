86976dd8a750d9fe4785951360d8c3556b84db6a
dennis <Dennis.Zhukki@gmail.com>
2021-12-18 18:01:37 +0100

    Seat: Seat Entität implementiert SeatStatus: SeatStatus ENUM implementiert

--- order-service/src/main/java/com/zeller/studrive/orderservice/model/Seat.java
+++ order-service/src/main/java/com/zeller/studrive/orderservice/model/Seat.java
@@ -1,4 +1,81 @@
 package com.zeller.studrive.orderservice.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Objects;
+
+@Document
 public class Seat {
+
+	@Id
+	private String id;
+	private Long passengerId;
+	private String rideId;
+	private SeatStatus seatStatus;
+
+	public Seat(Long passengerId, String rideId, SeatStatus seatStatus) {
+		this.passengerId = passengerId;
+		this.rideId = rideId;
+		this.seatStatus = seatStatus;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Long getPassengerId() {
+		return passengerId;
+	}
+
+	public void setPassengerId(Long passengerId) {
+		this.passengerId = passengerId;
+	}
+
+	public String getRideId() {
+		return rideId;
+	}
+
+	public void setRideId(String rideId) {
+		this.rideId = rideId;
+	}
+
+	public SeatStatus getSeatStatus() {
+		return seatStatus;
+	}
+
+	public void setSeatStatus(SeatStatus seatStatus) {
+		this.seatStatus = seatStatus;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o)
+			return true;
+		if(o == null || getClass() != o.getClass())
+			return false;
+
+		Seat seat = (Seat) o;
+
+		if(!Objects.equals(id, seat.id))
+			return false;
+		if(!Objects.equals(passengerId, seat.passengerId))
+			return false;
+		if(!Objects.equals(rideId, seat.rideId))
+			return false;
+		return seatStatus == seat.seatStatus;
+	}
+
+	@Override
+	public int hashCode() {
+		int result = id != null ? id.hashCode() : 0;
+		result = 31 * result + (passengerId != null ? passengerId.hashCode() : 0);
+		result = 31 * result + (rideId != null ? rideId.hashCode() : 0);
+		result = 31 * result + (seatStatus != null ? seatStatus.hashCode() : 0);
+		return result;
+	}
 }
